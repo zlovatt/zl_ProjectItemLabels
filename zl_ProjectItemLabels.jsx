cfa06f679a51a09db610dfc59e3564f249ae90f8
Zack Lovatt <zack@zacklovatt.com>
2017-02-19 17:07:50 -0800

    Code cleanup and scope encapsulation

--- zl_ProjectItemLabels.jsx
+++ zl_ProjectItemLabels.jsx
@@ -23,201 +23,143 @@
 
 **********************************************************************************************/
 
-    var zl_PIL__scriptName = "zl_ProjectItemLabels";
+    function zl_projectItemLabels (thisObj) {
+        var Config = {
+            name: "zl_ProjectItemLabels",
+            version: "1.0",
+        };
 
-    /******************************
-        zl_ProjectItemLabels()
+        function colourProjectItems (thisObj, curColour, objArray) {
+            var userItems = app.project.selection;
+            var noChecks = true;
 
-        Description:
-        This function contains the main logic for this script.
-
-        Parameters:
-        thisObj - "this" object.
-        curPos - position to put null
-        parentNull - bool, whether to parent null to layer
-        xOffset & yOffset - amount of pixels to shift null
-
-        Returns:
-        Nothing.
-    ******************************/
-    function zl_ProjectItemLabels(thisObj, curColour, objArray){
-        var userItems = app.project.selection;
-        var noChecks = true;
-
-        for (var i = 0, il = objArray.length; i < il; i++)
-            if (objArray[i].value){
-                noChecks = false;
-                break;
-            }
-
-        if (noChecks){ // If no item type is selected
-            alert("Select an item type!");
-        } else {
-            if (userItems.length == 0){ // If no items are selected
-                alert("Select at least one item!", zl_PIL__scriptName);
-            } else {
-                zl_ProjectItemLabels_colourItems (curColour, objArray, userItems, null);
-            }
-        }
-
-    } // end function CreateCornerNull
-
-
-    /******************************
-        zl_ProjectItemLabels_colourItems()
-
-        Description:
-        Colours items
-
-        Parameters:
-        curColour - label colour to use
-        objArray - array of objects to colour
-        userItems - collection of items to colour
-        folderLoc - location of folder item (for recursion)
-
-        Returns:
-        Nothing.
-     ******************************/
-    function zl_ProjectItemLabels_colourItems (curColour, objArray, userItems, folderLoc) {
-        var curLength = 0;
-        var startCount = 0;
-
-        curLength = userItems.length;
-
-        if (folderLoc !== null){
-            startCount = 1;
-            folderLoc ++;
-            curLength ++;
-        }
-
-        for (var i = startCount, il = curLength; i < il; i++){
-             var thisItem = userItems[i];
-
-            if (objArray[0].value){
-                thisItem.label = curColour;
-                if (thisItem instanceof FolderItem)
-                    zl_ProjectItemLabels_colourItems (curColour, objArray, thisItem.items, i);
-            } else {
-                if (thisItem instanceof FolderItem){ // is folder
-                    if (objArray[1].value)
-                        thisItem.label = curColour;
-                    zl_ProjectItemLabels_colourItems (curColour, objArray, thisItem.items, i);
+            for (var i = 0, il = objArray.length; i < il; i++)
+                if (objArray[i].value){
+                    noChecks = false;
+                    break;
                 }
 
-                if ((thisItem instanceof CompItem) && (objArray[3].value)) // is precomp
-                    thisItem.label = curColour;
-
-                if (thisItem instanceof FootageItem){
-                    if (!((thisItem.mainSource instanceof SolidSource) || (thisItem.mainSource instanceof PlaceholderSource)) && objArray[2].value){ // is footage
-                        thisItem.label = curColour;
-                    } else if ((thisItem.mainSource instanceof SolidSource) && (objArray[4].value)){ // is solid
-                        thisItem.label = curColour;
-                    } else if ((thisItem.mainSource instanceof PlaceholderSource) && (objArray[5].value)){ // is placeholder
-                        thisItem.label = curColour;
-                    }
-                }
-            }
-        }
-    } // end function colourItems
-
-
-    /******************************
-        zl_ProjectItemLabels_createPalette()
-
-        Description:
-        Creates ScriptUI Palette Panel
-        Generated using Boethos (crgreen.com/boethos)
-
-        Parameters:
-        thisObj - this comp object
-
-        Returns:
-        Nothing
-     ******************************/
-    function zl_ProjectItemLabels_createPalette(thisObj) {
-        var win = (thisObj instanceof Panel) ? thisObj : new Window('palette', 'Project Label Items',undefined);
-
-        { // Target
-            win.targetGroup = win.add('panel', undefined, 'Target', {borderStyle: "etched"});
-            win.targetGroup.alignChildren = "left";
-
-            win.targetGroup.allToggle = win.targetGroup.add('checkbox', undefined, '\u00A0All');
-            win.targetGroup.folderToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Folder');
-            win.targetGroup.footageToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Footage');
-            win.targetGroup.precompToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Precomp');
-            win.targetGroup.solidToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Solid');
-            win.targetGroup.placeholderToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Placeholder');
-
-            win.targetGroup.allToggle.value = true;
-            for (var i = 1, il = win.targetGroup.children.length; i < il; i++)
-                    win.targetGroup.children[i].enabled = false;
-
-            win.targetGroup.allToggle.onClick = function(){
-                for (var i = 1, il = win.targetGroup.children.length; i < il; i++)
-                    win.targetGroup.children[i].enabled = Math.abs(1-this.value);
-            }
-
-        }
-
-        { // Dropdown
-            var labelNameArray = ["None", "Red", "Yellow", "Aqua", "Pink", "Lavender", "Peach", "Sea Foam", "Blue", "Green", "Purple", "Orange", "Brown", "Fuschia", "Cyan", "Sandstone", "Dark Green"];
-            var curColour = 0;
-
-            win.colourPanel = win.add('panel', undefined, 'Colour');
-            win.colourPanel.colourList = win.colourPanel.add('dropdownlist', undefined, labelNameArray);
-            win.colourPanel.colourList.selection = 0;
-
-            win.colourPanel.colourList.onChange = function(){
-                curColour = win.colourPanel.colourList.selection.index;
-            }
-        } // end Dropdown
-
-        { // Buttons
-            win.colourButton = win.add('button', undefined, 'Set Labels');
-            win.colourButton.alignment = 'fill';
-
-            win.colourButton.onClick = function () {
-
-                if (app.project) {
-                    var userItems = app.project.selection;
-                    if (userItems != 0) {
-                        app.beginUndoGroup(zl_PIL__scriptName);
-                        zl_ProjectItemLabels(thisObj, curColour, win.targetGroup.children);
-                        app.endUndoGroup();
-                    } else {
-                        alert("Select at least one item!", zl_PIL__scriptName);
-                    }
+            if (noChecks){ // If no item type is selected
+                alert("Select an item type!");
+            } else {
+                if (userItems.length == 0){ // If no items are selected
+                    alert("Select at least one item!", Config.name);
                 } else {
-                    alert("Open a project!", zl_PIL__scriptName);
+                    doColourItem (curColour, objArray, userItems, null);
                 }
             }
-        } // end Buttons
+        };
 
-        if (win instanceof Window) {
-            win.show();
-        } else {
-            win.layout.layout(true);
-        }
-    } // end function createPalette
+        function doColourItem (curColour, objArray, userItems, folderLoc) {
+            var curLength = 0;
+            var startCount = 0;
 
+            curLength = userItems.length;
 
-    /******************************
-        zl_ProjectItemLabels_main()
+            if (folderLoc !== null){
+                startCount = 1;
+                folderLoc ++;
+                curLength ++;
+            }
 
-        Description:
-        Main function
+            for (var i = startCount, il = curLength; i < il; i++){
+                 var thisItem = userItems[i];
 
-        Parameters:
-        thisObj - this comp object
+                if (objArray[0].value){
+                    thisItem.label = curColour;
+                    if (thisItem instanceof FolderItem)
+                        doColourItem (curColour, objArray, thisItem.items, i);
+                } else {
+                    if (thisItem instanceof FolderItem){ // is folder
+                        if (objArray[1].value)
+                            thisItem.label = curColour;
+                        doColourItem (curColour, objArray, thisItem.items, i);
+                    }
 
-        Returns:
-        Nothing
-     ******************************/
-    function zl_ProjectItemLabels_main(thisObj) {
-        zl_ProjectItemLabels_createPalette(thisObj);
-    } // end function main
+                    if ((thisItem instanceof CompItem) && (objArray[3].value)) // is precomp
+                        thisItem.label = curColour;
 
+                    if (thisItem instanceof FootageItem){
+                        if (!((thisItem.mainSource instanceof SolidSource) || (thisItem.mainSource instanceof PlaceholderSource)) && objArray[2].value){ // is footage
+                            thisItem.label = curColour;
+                        } else if ((thisItem.mainSource instanceof SolidSource) && (objArray[4].value)){ // is solid
+                            thisItem.label = curColour;
+                        } else if ((thisItem.mainSource instanceof PlaceholderSource) && (objArray[5].value)){ // is placeholder
+                            thisItem.label = curColour;
+                        }
+                    }
+                }
+            }
+        };
+
+        function createPalette (thisObj) {
+            var win = (thisObj instanceof Panel) ? thisObj : new Window('palette', Config.name + " v" + Config.version, undefined);
+
+            { // Target
+                win.targetGroup = win.add('panel', undefined, 'Target', {borderStyle: "etched"});
+                win.targetGroup.alignChildren = "left";
+
+                win.targetGroup.allToggle = win.targetGroup.add('checkbox', undefined, '\u00A0All');
+                win.targetGroup.folderToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Folder');
+                win.targetGroup.footageToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Footage');
+                win.targetGroup.precompToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Precomp');
+                win.targetGroup.solidToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Solid');
+                win.targetGroup.placeholderToggle = win.targetGroup.add('checkbox', undefined, '\u00A0Placeholder');
+
+                win.targetGroup.allToggle.value = true;
+                for (var i = 1, il = win.targetGroup.children.length; i < il; i++)
+                        win.targetGroup.children[i].enabled = false;
+
+                win.targetGroup.allToggle.onClick = function(){
+                    for (var i = 1, il = win.targetGroup.children.length; i < il; i++)
+                        win.targetGroup.children[i].enabled = Math.abs(1-this.value);
+                }
+
+            }
+
+            { // Dropdown
+                var labelNameArray = ["None", "Red", "Yellow", "Aqua", "Pink", "Lavender", "Peach", "Sea Foam", "Blue", "Green", "Purple", "Orange", "Brown", "Fuschia", "Cyan", "Sandstone", "Dark Green"];
+                var curColour = 0;
+
+                win.colourPanel = win.add('panel', undefined, 'Colour');
+                win.colourPanel.colourList = win.colourPanel.add('dropdownlist', undefined, labelNameArray);
+                win.colourPanel.colourList.selection = 0;
+
+                win.colourPanel.colourList.onChange = function(){
+                    curColour = win.colourPanel.colourList.selection.index;
+                }
+            } // end Dropdown
+
+            { // Buttons
+                win.colourButton = win.add('button', undefined, 'Set Labels');
+                win.colourButton.alignment = 'fill';
+
+                win.colourButton.onClick = function () {
+
+                    if (app.project) {
+                        var userItems = app.project.selection;
+                        if (userItems != 0) {
+                            app.beginUndoGroup(Config.name);
+                            colourProjectItems(thisObj, curColour, win.targetGroup.children);
+                            app.endUndoGroup();
+                        } else {
+                            alert("Select at least one item!", Config.name);
+                        }
+                    } else {
+                        alert("Open a project!", Config.name);
+                    }
+                }
+            } // end Buttons
+
+            if (win instanceof Window) {
+                win.show();
+            } else {
+                win.layout.layout(true);
+            }
+        } // end function createPalette
+
+        createPalette(thisObj);
+    };
 
     // RUN!
-    //zl_ProjectItemLabels(this);
-    zl_ProjectItemLabels_main(this);
+    zl_projectItemLabels(this);
